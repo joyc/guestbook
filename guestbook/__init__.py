f89fa8aca90e5186f3d5d056de2c32f68b59f9fc
joyc <joyc@live.jp>
2019-01-06 12:35:03 +0900

    add debugtoolbar

--- guestbook/__init__.py
+++ guestbook/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask('guestbook')
 # app = Flask(__name__.split('.')[0])
@@ -12,5 +13,6 @@ app.jinja_env.lstrip_blocks = True
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+toolbar = DebugToolbarExtension(app)
 
 from guestbook import views, errors, commands
